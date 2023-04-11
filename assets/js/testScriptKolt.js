dda4631ae581f5bbc7e1732b0e2a31defdaf68ce
Elfelfa <koltbodzo@gmail.com>
2023-04-11 12:59:37 -0400

    Added fade-in function

---
+++ assets/js/testScriptKolt.js
@@ -0,0 +1,51 @@
+$(document).ready(
+    function() 
+    {
+        $("#startBtn").on("click", function () {fadeOut($("#test"))});   
+    }
+);
+
+function sceneTransition()
+{
+
+}
+
+function fadeOut (e)
+{
+    if (!e.css("opacity")) {
+        e.css("opacity", 1);
+    }
+
+    var fadeEffect = setInterval(function ()
+    {
+        if (parseFloat(e.css("opacity")).toFixed(1) < 0.1)
+        {
+            clearInterval(fadeEffect);
+        }
+        else
+        {
+            let i = parseFloat(e.css("opacity")).toFixed(1);
+            e.css("opacity", (i -= 0.1));
+        }
+    }, 50);
+}
+
+function fadeIn (e)
+{
+    if (!e.css("opacity")) {
+        e.css("opacity", 0);
+    }
+
+    var fadeEffect = setInterval(function ()
+    {
+        if (parseFloat(e.css("opacity")).toFixed(1) > 1)
+        {
+            clearInterval(fadeEffect);
+        }
+        else
+        {
+            let i = parseFloat(e.css("opacity")).toFixed(1);
+            e.css("opacity", (i += 0.1));
+        }
+    }, 50);
+}
