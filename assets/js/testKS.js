58108bfd8b7d4bee62eb1b0d4572f113fae5d880
Keely Sherman <keelysherman@Keelys-MacBook-Pro.local>
2023-04-12 11:25:30 -0400

    save so i can pull html to look

--- assets/js/testKS.js
+++ assets/js/testKS.js
@@ -15,23 +15,23 @@ fetch('API_URL')
 const cardsContainer = document.querySelector('#cards-container');
 
 function createCards(c) {
-    characters.forEach(c => {
+    c.forEach(cardData => {
       const div = document.createElement('div');
       const image = document.createElement('img');
       const name = document.createElement('h3');
-      const description = document.createElement('h3');
-      const choose = document.createElement('button');
+      //const description = document.createElement('h3');
+      const explore = document.createElement('button');
       div.classList = 'card'
       image.classList = 'card-img'
-      choose.classList = 'empty'
+      explore.classList = 'explore'
       image.src = card.image
       name.innerText = `City: ${name}`
-      description.innerText =`Discription: ${description}`
-      choose.textContent = 'choose'
+      //description.innerText =`Discription: ${description}`
+      explore.textContent = 'Explore'
       div.appendChild(image)
       div.appendChild(name)
-      div.appendChild(description)
-      div.appendChild(choose)
+      //div.appendChild(description)
+      div.appendChild(explore)
       cardsContainer.appendChild(div)
     });
   };
