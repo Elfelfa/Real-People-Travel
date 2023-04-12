77c844228ae5ed905ae61d3d3cf0e85b22b300b7
Keely Sherman <keelysherman@Keelys-MacBook-Pro.local>
2023-04-12 10:53:02 -0400

    create cards and todo priorities

---
+++ assets/js/testKS.js
@@ -0,0 +1,45 @@
+
+//add onclick="storeName()" to htmlbutton for storename
+function storeName(){
+    var inputName = document.getElementById("your-name").value;
+    localStorage.setItem("your-name", stringify(inputName));
+};
+function getName(){
+ var add
+}
+
+fetch('API_URL')
+.then(response => response.json())
+.then(data => console.log(data));
+
+const cardsContainer = document.querySelector('#cards-container');
+
+function createCards(c) {
+    characters.forEach(c => {
+      const div = document.createElement('div');
+      const image = document.createElement('img');
+      const name = document.createElement('h3');
+      const description = document.createElement('h3');
+      const choose = document.createElement('button');
+      div.classList = 'card'
+      image.classList = 'card-img'
+      choose.classList = 'empty'
+      image.src = card.image
+      name.innerText = `City: ${name}`
+      description.innerText =`Discription: ${description}`
+      choose.textContent = 'choose'
+      div.appendChild(image)
+      div.appendChild(name)
+      div.appendChild(description)
+      div.appendChild(choose)
+      cardsContainer.appendChild(div)
+    });
+  };
+
+ // function to store image into progress Bar upon click event (high priority)
+ // function to change card creater where description and button go on to top larger card (large card should be hard into html)(high priority)
+ // side bar function to open and closed like the high score thing (low priority)
+ // getname function (medium priority)
+ // kolt is doing hard api stuff 
+ // 
+
