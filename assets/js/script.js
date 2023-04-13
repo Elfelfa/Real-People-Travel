c43b53f62764603aaa1f77b03467b8db3c3d77fe
brandon6457 <brandon6457@gmail.com>
2023-04-13 12:12:23 -0400

    asside menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,13 @@
 //              
 ////////////////////////////////////////////////////
 
+function toggleSlideover(){
+   document.getElementById('slideover-container').classList.toggle('invisible');
+   document.getElementById('slideover-bg').classList.toggle('opacity-0');
+   document.getElementById('slideover-bg').classList.toggle('opacity-50');
+   document.getElementById('slideover').classList.toggle('translate-x-full');
+}
+
 //Global variables
 var currentPage = 0;
 var itinerary = [];
