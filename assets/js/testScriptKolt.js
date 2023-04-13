222a2783831417f871e5ce3b27001a7917f109c6
Elfelfa <koltbodzo@gmail.com>
2023-04-13 08:41:30 -0400

    Added functions to gather location data.

--- assets/js/testScriptKolt.js
+++ assets/js/testScriptKolt.js
@@ -10,18 +10,222 @@
 
 //Global variables
 var currentPage = 0;
+var chosenLocation = 'Paris';
+var tempLocations = [];
 var itinerary = [];
+
+const apiKey = '&key=AIzaSyDORJkJF8s_jJJqrMWshFrJTLxMXDFhTzg';
+const corsLink = 'https://cors-anywhere.herokuapp.com/';
+const placeLink = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?';
+const placeFields = '&inputtype=textquery&fields=formatted_address%2Cname%2Cphoto%2Crating%2Copening_hours%2Cgeometry%2Cprice_level';
+const photoLink = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&maxheight=400&photoreference=';
+const nearbyLink = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?';
+
+getStarterLocation();
+
 
 //script.js will not execute any code until the page's DOM nodes are ready.
 //
-$(document).ready(
-    function() 
-    {
-        document.getElementById("#test").addEventListener("click", function(){
-            fadeOut()
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("test").addEventListener("click", function(){
+        fadeOut(this);
+        sceneTransition();
+    });
+});
+
+// corsLink + placeLink = https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/findplacefromtext/json?
+// chosenLocation is the location the user places into the input box.
+// placeFields = &inputtype=textquery&fields=formatted_address%2Cname%2Cphoto%2Crating%2Copening_hours%2Cgeometry%2Cprice_level
+//
+// placeFields query will grab the:
+//      Formatted address
+//      Location name
+//      Photo reference
+//      Lattitude and longitude
+//      Location rating if applicable
+//      Opening hours if applicable 
+//      Price level if applicable
+//
+function getStarterLocation ()
+{
+    //chosenLocation = document.getElementById('placeSearch').value.trim();
+
+    var locData = {
+        address: '',
+        name: '',
+        image: '',
+        lat: '',
+        lon: ''
+    };
+
+    // corsLink + photoLink = https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&maxheight=400&photoreference=
+    // Grabs photo reference to pull an image from the places API.
+    //
+    fetch(corsLink + placeLink + 'input=' + chosenLocation + placeFields + apiKey)
+            .then(function(response) {
+                if(response.ok)
+                {
+                    console.log(response)
+
+                    response.json().then(function (data) {
+                        console.log(data);
+                        locData.address = data.candidates[0].formatted_address;
+                        locData.lat = data.candidates[0].geometry.location.lat;
+                        locData.lon = data.candidates[0].geometry.location.lng;
+                        locData.name = data.candidates[0].name;
+                        locData.image = corsLink + photoLink + data.candidates[0].photos[0].photo_reference + apiKey;
+                    });
+                };
+            });
+    itinerary.push(locData);
+    console.log(itinerary);
+    setTimeout(getNearbyHotels, 5000);
+}
+
+// corsLink + placeLink = https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?
+//  keyword = place category (hotel, monument, restaurant, museum, etc.)
+//  location = lat%lon
+//  radius in meters
+//  
+//  This will return an array of 20 results
+//
+function getNearbyHotels ()
+{
+    console.log('Start getNearbyHotels');
+    fetch(corsLink + nearbyLink + 'keyword=hotel&location=' + itinerary[0].lat + '%2C' + itinerary[0].lon + '&radius=50000' + apiKey)
+        .then(function(response) {
+            if(response.ok)
+            {
+                console.log(response);
+
+                response.json().then(function (data) {
+                    console.log(data);
+                    setTimeout('', 5000);
+
+                    data.results.forEach(loc => {
+                        var locData = {
+                            address: '',
+                            name: '',
+                            image: '',
+                            lat: '',
+                            lon: ''
+                        }
+
+                        if (loc.address)
+                        {
+                            locData.address = loc.formatted_address;
+                            
+                        }
+                        else if (loc.vicinity)
+                        {
+                            locData.address = loc.vicinity;
+                        };
+    
+                        locData.name = loc.name;
+                        locData.image = corsLink + photoLink + loc.photos[0].photo_reference + apiKey;
+                        locData.lat = loc.geometry.location.lat;
+                        locData.lon = loc.geometry.location.lng;
+                
+                        tempLocations.push(locData);
+                    });
+
+                    console.log(tempLocations);
+                    itinerary.push(tempLocations[0]);
+                    console.log('End getNearbyHotels');
+                });
+            };
         });
-    }
-);
+    
+    setTimeout(getNearbyAttractions, 5000);
+}
+
+function getNearbyAttractions ()
+{
+    
+    console.log('Start getNearbyAttractions');
+
+    tempLocations.length = 0;
+
+    var i = 0;
+
+    var myBuffer = setInterval(function () 
+    {
+        if (i < 4)
+        {
+            switch (i){
+                case 0:
+                    var currentAttr = 'tourist_attraction';
+                    break;
+                case 1:
+                    var currentAttr = 'cafe';
+                    break;
+                case 2:
+                    var currentAttr = 'art_gallery';
+                    break;
+                case 3:
+                    var currentAttr = 'museum';
+                    break;
+            }
+
+            console.log('buffering');
+            console.log('i = ' + i);
+            
+            fetch(corsLink + nearbyLink + 'keyword=' + currentAttr + '&location=' + itinerary[1].lat + '%2C' + itinerary[1].lon + '&radius=50000' + apiKey)
+            .then(function(response) {
+                if(response.ok)
+                {
+                    response.json().then(function (data) {
+                        console.log(data);
+                        setTimeout('', 5000);
+                            
+                        data.results.forEach(loc => {
+                                
+                            var locData = {
+                                address: '',
+                                name: '',
+                                image: '',
+                                lat: '',
+                                lon: ''
+                            }
+                                
+                            if (loc.business_status !== 'CLOSED_PERMANENTLY')
+                            {
+                                if (loc.address)
+                                {
+                                    locData.address = loc.formatted_address;
+                                        
+                                }
+                                else if (loc.vicinity)
+                                {
+                                    locData.address = loc.vicinity;
+                                };
+                        
+                                locData.name = loc.name;
+
+                                if (locData.photos)
+                                {
+                                    locData.image = corsLink + photoLink + loc.photos[0].photo_reference + apiKey;
+                                };
+                                
+                                locData.lat = loc.geometry.location.lat;
+                                locData.lon = loc.geometry.location.lng;
+                        
+                                tempLocations.push(locData);
+                            }
+                        });
+                    });
+                };
+            });
+            i++;
+        }
+        else
+        {
+            clearInterval(myBuffer);
+            console.log(tempLocations);
+            console.log('End getNearbyAttractions');
+        }
+    }, 5000);
+}
 
 //Switch case function that will determine what scene the page will
 //transition to.
@@ -38,7 +242,6 @@ function sceneTransition()
 
     switch(currentPage){
         case 1:
-            //fade out elements for splash
             //change elements to input page
             //fade back in the page
             break;
@@ -81,12 +284,12 @@ function sceneTransition()
 function fadeOut (e)
 {
     if (!e.style.opacity) {
-        e.style.opacity = 0;
+        e.style.opacity = 1;
     }
 
     var fadeEffect = setInterval(function ()
     {
-        if (e.style.opacity > 1)
+        if (e.style.opacity <= 0)
         {
             clearInterval(fadeEffect);
         }
@@ -105,7 +308,7 @@ function fadeIn (e)
 
     var fadeEffect = setInterval(function ()
     {
-        if (e.style.opacity > 1)
+        if (e.style.opacity >= 1)
         {
             clearInterval(fadeEffect);
         }
@@ -115,6 +318,8 @@ function fadeIn (e)
         }
     }, 50);
 }
+
+
 
 //  Create function populateCards()
 //
